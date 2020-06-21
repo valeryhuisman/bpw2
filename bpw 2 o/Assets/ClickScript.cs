a9e6180e297955c65261ae11fafadcfae73e3705
valeryhuisman <valeryhuisman@gmail.com>
2020-06-21 16:34:48 +0200

    backup bpw2

---
+++ bpw 2 o/Assets/ClickScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 2f) && Input.GetMouseButtonDown(0))
+        {
+            if(hit.transform.tag == "SceneChanger")
+            {
+                Debug.Log("hit boui");
+                SceneTransitionScript sceneTransitionScript = hit.transform.gameObject.GetComponent<SceneTransitionScript>();
+                StartCoroutine(sceneTransitionScript.SceneTransitionRoutine());
+            }
+        }
+    }
+}
---
+++ bpw 2 o/Assets/SceneTransitionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionScript : MonoBehaviour
+{
+    public string nextSceneName;
+    public Animator transition;
+    public Animator turnAnimation;
+    public float transitionTime;
+
+    private void Start()
+    {
+        gameObject.tag = "SceneChanger";
+    }
+
+    
+    public IEnumerator SceneTransitionRoutine()
+    {
+        turnAnimation.SetTrigger("StartDraai");
+        yield return new WaitForSeconds(2);
+        transition.SetTrigger("Fade");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
